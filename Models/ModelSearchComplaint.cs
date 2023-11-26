707ca1ae03d86a77619881cf367e36c6ae2203a4
ajayPrismHR <107325460+ajayPrismHR@users.noreply.github.com>
2023-11-26 13:22:36 +0530

    initial

---
+++ Models/ModelSearchComplaint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static ComplaintTracker.JqueryDatatableParam;
+
+namespace ComplaintTracker.Models
+{
+    public class ModelSearchComplaint : DataTableAjaxPostModel
+    {
+        public string NAME { get; set; }
+        public string COMPLAINT_DATE { get; set; }
+        public string COMPLAINT_NO { get; set; }
+        public string LineMan_Name { get; set; }
+        public string REMARK { get; set; }
+        public string ADDRESS { get; set; }
+        public string COMPLAINT_TYPE { get; set; }
+        public string Consumer_no { get; set; }
+        public string MOBILE_NO { get; set; }
+        public Int64 COMPLAINT_NO1 { get; set; }
+
+    }
+}
